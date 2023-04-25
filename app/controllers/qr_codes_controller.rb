5e5f9752576301274e34bf6da6f76a263956b8c9
Josseph Downs <downsjosseph@gmail.com>
2023-04-25 16:24:19 +0000

    created show

--- app/controllers/qr_codes_controller.rb
+++ app/controllers/qr_codes_controller.rb
@@ -1,14 +1,29 @@
-class QrCodesController < ApplicationController
+class QRCodesController < ApplicationController
   require 'rqrcode'
   require 'rqrcode_svg'
 
   def new
+    @qr_code_info = QrCodeInfo.new
   end
 
+  def show
+    @qr_code_info = QrCodeInfo.find(params[:id])
+  end
+  
+
   def create
-    qr_data = params.slice(:first_name, :last_name, :city, :address, :zip, :state, :job_place, :job_description).to_json
+    @qr_code_info = QrCodeInfo.new(qr_code_info_params)
+    @qr_code_info.save
+
+    qr_data = qr_code_info_params.except(:info_type).to_json
     @qr_code = RQRCode::QRCode.new(qr_data)
     @qr_code_svg = @qr_code.as_svg(offset: 0, color: '000', shape_rendering: 'crispEdges', module_size: 6)
     send_data @qr_code_svg.to_s, type: 'image/svg+xml', disposition: 'inline'
+  end
+
+  private
+
+  def qr_code_info_params
+    params.require(:qr_code_info).permit(:first_name, :last_name, :city, :address, :zip, :state, :job_place, :job_description, :wifi_ssid, :wifi_password, :info_type)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :qr_codes, only: [:new, :create]
+  resources :qr_codes, only: [:new, :create, :show]
   root to: 'qr_codes#new'
 end
