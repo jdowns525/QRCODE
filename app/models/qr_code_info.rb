6af531b7cf3af22d336d8aeb10f4230a4686e5c9
Josseph Downs <downsjosseph@gmail.com>
2023-04-25 16:32:10 +0000

    created navbar

--- app/models/qr_code_info.rb
+++ app/models/qr_code_info.rb
@@ -18,4 +18,15 @@
 #  updated_at      :datetime         not null
 #
 class QrCodeInfo < ApplicationRecord
+  validates :info_type, presence: true
+  validates :first_name, :last_name, :city, :address, :zip, :state, :job_place, :job_description, presence: true, if: :business_card?
+  validates :wifi_ssid, :wifi_password, presence: true, if: :wifi?
+
+  def business_card?
+    info_type == 'Business Card'
+  end
+
+  def wifi?
+    info_type == 'Wi-Fi'
+  end
 end
