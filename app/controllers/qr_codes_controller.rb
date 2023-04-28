ec922bf293cc5a8372ddcb0989882d1fd8d032bb
Josseph Downs <downsjosseph@gmail.com>
2023-04-28 18:07:14 +0000

    commit

--- app/controllers/qr_codes_controller.rb
+++ app/controllers/qr_codes_controller.rb
@@ -12,25 +12,9 @@ class QrCodesController < ApplicationController
 
   def show
     @qr_code_info = QrCodeInfo.find(params[:id])
-    qr_data = {
-      id: @qr_code_info.id,
-      first_name: @qr_code_info.first_name,
-      last_name: @qr_code_info.last_name,
-      city: @qr_code_info.city,
-      address: @qr_code_info.address,
-      zip: @qr_code_info.zip,
-      state: @qr_code_info.state,
-      job_place: @qr_code_info.job_place,
-      job_description: @qr_code_info.job_description,
-      github_link: @qr_code_info.github_link,
-      personal_website_link: @qr_code_info.personal_website_link
-    }.to_json
-    @qr_code = RQRCode::QRCode.new(qr_data)
-    @qr_code_svg = @qr_code.as_svg(offset: 0, color: '000', shape_rendering: 'crispEdges', module_size: 6)
+    @qr_code_svg = generate_qr_code(@qr_code_info)
   end
   
-  
-
   def create
     @qr_code_info = QrCodeInfo.new(qr_code_info_params)
     @qr_code_info.save
@@ -51,15 +35,33 @@ class QrCodesController < ApplicationController
       qr_code_url: qr_code_url
     }.to_json
 
-    @qr_code = RQRCode::QRCode.new(qr_data)
-    @qr_code_svg = @qr_code.as_svg(offset: 0, color: '000', shape_rendering: 'crispEdges', module_size: 6)
+    @qr_code_svg = generate_qr_code(@qr_code_info)
     send_data @qr_code_svg.to_s, type: 'image/svg+xml', disposition: 'inline'
   end
 
   private
+
+  def generate_qr_code(qr_code_info)
+    qr_data = {
+      id: qr_code_info.id,
+      first_name: qr_code_info.first_name,
+      last_name: qr_code_info.last_name,
+      city: qr_code_info.city,
+      address: qr_code_info.address,
+      zip: qr_code_info.zip,
+      state: qr_code_info.state,
+      job_place: qr_code_info.job_place,
+      job_description: qr_code_info.job_description,
+      github_link: qr_code_info.github_link,
+      personal_website_link: qr_code_info.personal_website_link
+    }.to_json
+
+    RQRCode::QRCode.new(qr_data).as_svg(offset: 0, color: '000', shape_rendering: 'crispEdges', module_size: 6)
+  end
 
   def qr_code_info_params
     params.require(:qr_code_info).permit(:first_name, :last_name, :city, :address, :zip, :state, :job_place, :job_description, :wifi_ssid, :wifi_password, :info_type, :github_link, :personal_website_link)
   end
   
 end
+
